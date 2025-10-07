61f348ef7c0f3c07b94577f056d301b570864c6f
John Nolan <90651514+Fediry@users.noreply.github.com>
2025-10-07 01:28:07 -0700

    trying to figure out converting the mantissa

--- inc/ieee_754.h
+++ inc/ieee_754.h
@@ -9,6 +9,9 @@ uint8_t const width = 32U;
 uint8_t const exp_width = 8U;
 uint8_t const mantissa_width = width - exp_width - 1;
 uint8_t const bias = 127U;
+uint32_t const sign_mask = 0xFFFFFFFF;
+uint32_t const exp_mask = 0x7F800000;
+uint32_t const mantissa_mask = 0x7FFFFF;
 
 class ieee754 {
    private:
@@ -16,6 +19,7 @@ class ieee754 {
     uint32_t bitwise_add(uint32_t a, uint32_t b);
     bool read_bit(uint32_t data, uint32_t n);
     uint32_t read_bit_segments(uint32_t data, uint32_t mask, uint32_t shift);
+    uint32_t conv_mantissa(uint32_t raw_mantissa);
 
    public:
     ieee754() {}
--- src/ieee_754.cpp
+++ src/ieee_754.cpp
@@ -36,45 +36,35 @@ uint32_t ieee754::bitwise_add(uint32_t a, uint32_t b) {
 
 bool ieee754::read_bit(uint32_t data, uint32_t n) { return data & (1 << n); }
 uint32_t ieee754::read_bit_segments(uint32_t data, uint32_t mask, uint32_t shift) {
-    // uint32_t shifted_data = data >> start;
-    // cout << " shifted_data: " << shifted_data;
-
-    // uint32_t mask = (1 << end) - 1;
-    // cout << "  mask: " << mask;
-
-    // uint32_t result = shifted_data & mask;
-    // cout << "  result: " << result << endl;
-
-    uint32_t sign_mask = 0xFFFFFFFF;
-    uint32_t sign = data & sign_mask;
-    cout << "sign: " << (sign >> 31);
-    uint32_t exp_mask = 0x7F800000;
-    uint32_t result = data & exp_mask;
-    cout << "  exp: " << ((result >> 23) - 127);
-    uint32_t mantissa_mask = 0x7FFFFF;
-    uint32_t result_m = data & mantissa_mask;
-    cout << "  mantissa: " << (result_m) << endl;
-
+    uint32_t result = data & mask;
+    result = result >> shift;
     return result;
 }
 
+uint32_t conv_mantissa(uint32_t raw_mantissa) {}
+
 float ieee754::ieee_754(uint32_t const data) {
     float value;
-    bool sign_bit = read_bit(data, width - 1);
-    cout << "sign_bit: " << sign_bit << endl;
+    // bool sign_bit = read_bit(data, width - 1);
+    bool sign_bit = read_bit_segments(data, sign_mask, width - 1);
+    cout << "sign_bit: " << sign_bit;
 
-    uint8_t raw_exponent = read_bits(data, 1, exp_width);
-    uint8_t exponent = raw_exponent - bias;
+    uint32_t raw_exponent = read_bit_segments(data, exp_mask, mantissa_width);
+    uint32_t exponent = raw_exponent - bias;
+    cout << " exp: " << exponent;
 
-    uint32_t mantissa;
+    uint32_t raw_mantissa = read_bit_segments(data, mantissa_mask, 0);
+    cout << " mantissa: " << raw_mantissa;
+
+    uint32_t mantissa = conv_mantissa(raw_mantissa);
 
     // if exp = 0 and mant != 0, use denormalized form {mantissa leading bit = 0}
-    uint8_t leading_m_bit = 1;
+    uint32_t leading_m_bit = 1;
     if (exponent == 0 && mantissa != 0) {
         leading_m_bit = 0;
     }
 
-    // value = (2 ^ exponent) * (-1 ^ sign_bit) * (leading_m_bit + mantissa);
-    value = -1.5;
+    value = (-1 ^ sign_bit) * (2 ^ exponent) * (leading_m_bit + mantissa);
+    cout << " value: " << value << endl;
     return value;
 }
