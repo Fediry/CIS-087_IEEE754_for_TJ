291c0e365d976a59860e59a2bfb80a4535a1b7b3
John Nolan <90651514+Fediry@users.noreply.github.com>
2025-10-10 23:42:24 -0700

    did some experiments, and ultimately scrapped the bitwise_add/mult/pow functions as I could not get them to work with negative numbers

--- src/ieee_754.cpp
+++ src/ieee_754.cpp
@@ -2,40 +2,6 @@
 
 using namespace std;
 
-/* Adding a and b by XORing them for the base add, and getting the carry by ANDing them and
- * left shifting that by 1. When b is zero, a is the result.
- */
-uint32_t ieee754::bitwise_add(uint32_t a, uint32_t b) {
-    uint32_t result;
-
-    while (b > 0U) {
-        uint32_t result = a ^ b;
-        uint32_t carry = (a & b) << 1U;
-        a = result;
-        b = carry;
-    }
-    return result;
-}
-
-/* Multiply a and b by doubling a and halfing b until b is zero.
- * When b is odd during this process, add a to the result.
- */
-uint32_t ieee754::bitwise_multiply(uint32_t a, uint32_t b) {
-    uint32_t result = 0U;
-
-    while (b > 0U) {
-        if (b & 1U) {
-            result += a;
-        }
-
-        a = a << 1U;
-        b = b >> 1U;
-    }
-    return result;
-}
-
-uint32_t ieee754::bitwise_power(uint32_t a, uint32_t b) {}
-
 // read bits from data at mask bits and shift right to pull out segments
 uint32_t ieee754::read_bit_segments(uint32_t const &data, uint32_t const mask, uint32_t const shift) {
     uint32_t result = data & mask;
@@ -64,7 +30,7 @@ float ieee754::ieee_754(uint32_t const &data) {
 
     uint8_t leading_bit = 1;
 
-    float mantissa = (leading_bit + raw_mantissa * pow(2U, -mantissa_width));
-    float output_float = sign * (mantissa)*pow(2U, exponent);
+    float mantissa = leading_bit + (raw_mantissa * pow(2, -mantissa_width));
+    float output_float = sign * (mantissa)*pow(2, exponent);
     return output_float;
 }
