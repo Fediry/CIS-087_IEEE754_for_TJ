fb9d52cbe0e3ea96d174a154bb560a97b219ff8d
John Nolan <90651514+Fediry@users.noreply.github.com>
2025-10-24 22:52:01 -0700

    removed comments to the right of lines

--- inc/ieee_754.h
+++ inc/ieee_754.h
@@ -12,21 +12,21 @@ uint8_t const bias = 127U;
 
 // Bit masks mapped to the segments of the IEEE 754 32-bit binary
 // (Used hex for ease of reading)
-uint32_t const sign_mask = 0xFFFFFFFF;       // 32'nd bit
-uint32_t const exp_mask = 0x7F800000;        // 31'st to 23'rd bits
-uint32_t const mantissa_mask = 0x7FFFFF;     // 22'nd to 0'th bits
-uint32_t const leading_bit_mask = 0x800000;  // 24'th bit of mantissa (implicit bit)
+uint32_t const sign_mask = 0xFFFFFFFF;
+uint32_t const exp_mask = 0x7F800000;
+uint32_t const mantissa_mask = 0x7FFFFF;
+uint32_t const leading_bit_mask = 0x800000;
 
 class ieee754 {
    private:
-    uint32_t read_bit_segments(uint32_t const &data, uint32_t const mask, uint32_t const shift);
-    uint32_t get_sign(uint32_t const &data);
-    int32_t get_exponent(uint32_t const &data);
-    uint32_t get_raw_mantissa(uint32_t const &data);
+    uint32_t read_bit_segments(uint32_t const& data, uint32_t const mask, uint32_t const shift);
+    uint32_t get_sign(uint32_t const& data);
+    int32_t get_exponent(uint32_t const& data);
+    uint32_t get_raw_mantissa(uint32_t const& data);
 
    public:
     ieee754() {}
     // using rule of zero
 
-    float ieee_754(uint32_t const &data);
+    float ieee_754(uint32_t const& data);
 };
